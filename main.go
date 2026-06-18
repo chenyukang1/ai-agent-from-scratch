4e0a306a5514626155cf71fdd5dccc85e1a4aa9a
tony <chenyukang259@qbitnetwork.com>
2026-06-18 16:14:11 +0800

    fix(chatmodel): main use acl api

--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"fmt"
-	"io"
 	"net/http"
 	"os"
-	"time"
 
+	"github.com/chenyukang1/ai-agent-from-scratch/internal/components/chat/openai"
+	"github.com/chenyukang1/ai-agent-from-scratch/internal/schema"
 	"github.com/joho/godotenv"
-	"github.com/sashabaranov/go-openai"
 )
 
 func main() {
@@ -26,61 +24,31 @@ func main() {
 		return
 	}
 
-	config := openai.DefaultConfig(apiKey)
 	baseURL := os.Getenv("OPENAI_BASE_URL")
-	if baseURL != "" {
-		config.BaseURL = baseURL
-	}
-
-	config.HTTPClient = &http.Client{
-		Transport: &headerTransport{
-			rt: http.DefaultTransport,
-		},
-		Timeout: 10 * time.Second,
-	}
-
-	client := openai.NewClientWithConfig(config)
-	resp, err := client.CreateChatCompletion(
-		context.Background(),
-		openai.ChatCompletionRequest{
-			Model: "deepseek/deepseek-v4-flash",
-			Messages: []openai.ChatCompletionMessage{
-				{
-					Role:    openai.ChatMessageRoleUser,
-					Content: "Hello!",
-				},
-			},
-		},
-	)
-	if err != nil {
-		fmt.Printf("ChatCompletion error: %v\n", err)
+	if baseURL == "" {
+		fmt.Println("OPENAI_BASE_URL is not set")
 		return
 	}
 
-	fmt.Println(resp.Choices[0].Message.Content)
-}
-
-type headerTransport struct {
-	rt http.RoundTripper
-}
-
-func (h *headerTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	// req.Header.Set("HTTP-Referer", "https://test.com")
-	// req.Header.Set("X-OpenRouter-Title", "test")
-	req.Header.Set("User-Agent", "curl/7.81.0")
-	req.Header.Set("Accept", "*/*")
-	// 打印 URL
-	fmt.Printf("Request URL: %s %s\n", req.Method, req.URL.String())
-	// 打印 Headers
-	for k, v := range req.Header {
-		fmt.Printf("Header: %s = %v\n", k, v)
-	}
-	// 打印 Body
-	if req.Body != nil {
-		bodyBytes, _ := io.ReadAll(req.Body)
-		req.Body = io.NopCloser(bytes.NewBuffer(bodyBytes)) // 写回 Body
-		fmt.Printf("Body: %s\n", string(bodyBytes))
+	config := openai.ChatModelConfig{
+		APIKey:     apiKey,
+		BaseURL:    baseURL,
+		Model:      "deepseek/deepseek-v4-flash",
+		HTTPClient: http.DefaultClient,
 	}
 
-	return h.rt.RoundTrip(req)
+	chatmodel := openai.NewChatModel(&config)
+	input := []*schema.Message{
+		{
+			Role:    "user",
+			Content: "What is the capital of France?",
+		},
+	}
+	resp, err := chatmodel.Generate(context.Background(), input)
+	if err != nil {
+		fmt.Printf("Error generating response: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Response: %s\n", resp.Content)
 }
